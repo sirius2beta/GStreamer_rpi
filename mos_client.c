fb42dd1e7463ad07dd6f63a6a23a63410153df04
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-01-19 17:10:44 +0800

    Update mos_client.c

--- mos_client.c
+++ mos_client.c
@@ -21,13 +21,13 @@ void on_connect(struct mosquitto *mosq, void *obj, int rc) {
 }
 
 void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_message *msg) {
-	CustomData* data = (CustomData*) obj
+	CustomData* data = (CustomData*) obj;
 	printf("New message with topic %s: %s\n", msg->topic, (char *) msg->payload);
 	data->pipeline = gst_parse_launch("gst-launch-1.0 -v v4l2src device=/dev/video0 num-buffers=-1 ! video/x-raw, width=160, height=120, framerate=12/1 ! videoconvert ! jpegenc ! rtpjpegpay ! udpsink host=10.8.0.4 port=5200",
       NULL);
 	gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
 	data->bus = gst_element_get_bus (pipeline);
-  	data->msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+  	data->msg = gst_bus_timed_pop_filtered (data->bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
   /* See next tutorial for proper error message handling/parsing */
   if (GST_MESSAGE_TYPE (msg) == GST_MESSAGE_ERROR) {
