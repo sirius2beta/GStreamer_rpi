566d48b90b8ac7331ac3d658cb0da3b9b9edd811
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2022-01-19 18:55:11 +0800

    Update mos_client.c

--- mos_client.c
+++ mos_client.c
@@ -26,10 +26,10 @@ void on_message(struct mosquitto *mosq, void *obj, const struct mosquitto_messag
 	printf("New message with topic %s: %s\n", msg->topic, message);
 	if(strcmp(message,"play") == 0){
 		printf("Playing...");
-		if(streaming_started == false){
+		if(data->streaming_started == false){
 			data->pipeline = gst_parse_launch("gst-launch-1.0 -v v4l2src device=/dev/video0 num-buffers=-1 ! video/x-raw, width=640, height=480, framerate=12/1 ! videoconvert ! jpegenc ! rtpjpegpay ! udpsink host=10.8.0.4 port=5200", NULL);
 			gst_element_set_state (data->pipeline, GST_STATE_PLAYING);
-			streaming_started = true;
+			data->streaming_started = true;
 		}
 	}
 }
